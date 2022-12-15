c07aa2a01622e5b542705004829d9610dae5cb9b
Emir Marat <emmarrat@gmail.com>
2022-12-15 17:09:16 +0100

    Created possibility to check entered password
    Created new checkResult reducer action, which checks selected pass with the const correctPass.

--- src/containers/PassChecker/PassChecker.tsx
+++ src/containers/PassChecker/PassChecker.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import './PassChecker.css';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../app/store";
-import {remove, result} from "./passCheckerSlice";
+import {checkResult, remove, result} from "./passCheckerSlice";
 
 const PassChecker = () => {
   const dispatch = useDispatch();
   const digits = useSelector((state: RootState) => state.passChecker.digits);
   const display = useSelector((state: RootState) => state.passChecker.result);
+  const passStatus = useSelector((state: RootState) => state.passChecker.checker);
 
-  console.log(display);
-
-  let displayContent = (<div className="display">{display}</div>);
+  let displayContent = <div className="display">{display.map(() => '*')}</div>;
 
   if(display.length === 0) {
     displayContent = <div className="display">Please enter the password</div>
+  }
+  if(passStatus !== null) {
+    displayContent = <div className={passStatus ? "correct display" : "wrong display"}>{passStatus ? 'Correct password!' : 'Wrong password!'}</div>
   }
 
   return (
@@ -25,7 +27,7 @@ const PassChecker = () => {
             <div onClick={() => dispatch(result(index))} className="button" key={digit}>{digit}</div>
         ))}
         <div onClick={() => dispatch(remove())} className="button">remove</div>
-        <div className="button">enter</div>
+        <div onClick={() => dispatch(checkResult())} className="button">enter</div>
       </div>
 
     </div>
--- src/containers/PassChecker/passCheckerSlice.ts
+++ src/containers/PassChecker/passCheckerSlice.ts
@@ -3,25 +3,36 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 interface CounterState {
   digits: number[];
   result: number[];
+  checker: boolean | null;
 }
 
 const initialState: CounterState = {
   digits: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
   result: [],
+  checker: null,
 };
+
+const correctPass = '1234';
 
 export const passCheckerSlice = createSlice({
   name: 'passChecker',
   initialState,
   reducers: {
     result: (state, action: PayloadAction<number>) => {
-     state.result.push(state.digits[action.payload]);
+      if(state.result.length < 4) {
+        state.result.push(state.digits[action.payload]);
+      }
     },
     remove: (state) => {
       state.result.pop();
-    }
+      state.checker = null;
+    },
+    checkResult: (state) => {
+      const finalResult = state.result.join('');
+      state.checker = finalResult === correctPass;
+    },
   }
 });
 
 export const passCheckerReducer = passCheckerSlice.reducer;
-export const {result, remove} = passCheckerSlice.actions;
+export const {result, remove, checkResult} = passCheckerSlice.actions;
